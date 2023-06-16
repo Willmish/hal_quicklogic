c12e7ccb2da626c53f264e7109ff51324fe4c9da
Szymon Duchniewicz <szduchniewicz@gmail.com>
2023-06-16 15:04:19 +0100

    Update CMakeLists.txt * Update Copyright.
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

--- HAL/src/eoss3_hal_wb.c
+++ HAL/src/eoss3_hal_wb.c
@@ -2,6 +2,7 @@
  * ==========================================================
  *
  *    Copyright (C) 2020 QuickLogic Corporation             
+ *    Copyright (C) 2023 Szymon Duchniewicz
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
